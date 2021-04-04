89de15fd41109273a548daea73e30e56091f3b87
TheInfogamer <81807943+TheInfogamer@users.noreply.github.com>
2021-04-04 11:39:38 +0530

    Main App

---
+++ Store Sim Files/storesim.py
@@ -0,0 +1,13 @@
+# Importing modules while printing messages to create illusion of loading
+
+print("Loading interface...")
+import storesimmodules as lib
+con=lib.con
+
+
+print('''============================================================================================================
+============================================================================================================
+                                                ---Welcome!---''')
+
+lib.welcome()
+lib.login()
